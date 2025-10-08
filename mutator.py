d7b34eff9ea6d5bf1167b45019af2e6541b56272
Eric Grahn <eric.grahn@chasacademy.se>
2025-10-08 12:15:55 +0200

    added 15 second update

--- mutator.py
+++ mutator.py
@@ -14,7 +14,7 @@ def main(url, station):
              'Accept': "application/vnd.pagerduty+json;version=2"}
         P = requests.put(url+'/reg', data=D, headers=H)
         print(P.status_code)
-        time.sleep(10)
+        time.sleep(15)
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
